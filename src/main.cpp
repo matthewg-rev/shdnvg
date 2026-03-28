e14f1b21e8cd2e43a2e5eb0b61ac159b32215365
mattgarabedian2015@gmail.com <130177336+matthewg-rev@users.noreply.github.com>
2026-03-27 19:28:54 -0700

    Added libcamera header files...

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,17 @@
+#include <libcamera/libcamera.h>
+#include <libcamera/camera.h>
+#include <libcamera/camera_manager.h>
+#include <libcamera/control_ids.h>
+#include <libcamera/controls.h>
+#include <libcamera/formats.h>
+#include <libcamera/framebuffer.h>
+#include <libcamera/request.h>
+#include <libcamera/stream.h>
+#include <libcamera/transform.h>
 #include <iostream>
+#include <fstream>
+#include <chrono>
+#include <thread>
 
 const int OLED_WIDTH = 1920;
 const int OLED_HEIGHT = 1080;
