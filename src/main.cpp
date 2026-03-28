9cf83c17563ae551ef0e6f69593e0a0e8375908b
mattgarabedian2015@gmail.com <130177336+matthewg-rev@users.noreply.github.com>
2026-03-27 21:42:32 -0700

    Trying something simple and different.

--- src/main.cpp
+++ src/main.cpp
@@ -15,45 +15,8 @@
 const int OLED_WIDTH = 1920;
 const int OLED_HEIGHT = 1080;
 
-class IMX290Camera {
-private: 
-    std::unique_ptr<libcamera::CameraManager> cameraManager_;
-    std::shared_ptr<libcamera::Camera> camera_;
-public:
-    IMX290Camera() {
-        cameraManager_ = std::make_unique<libcamera::CameraManager>();
-        int result = cameraManager_->start();
-        if (result != 0) {
-            throw std::runtime_error("failed to start camera manager: " + std::to_string(result));
-        }
-
-        auto cameras = cameraManager_->cameras();
-        if (cameras.empty()) {
-            throw std::runtime_error("no cameras found");
-        }
-
-        // attempt to acquire the camera
-        camera_ = cameras[0]; // our hardware setup should only have one camera
-        result = camera_->acquire();
-        if (result != 0) {
-            throw std::runtime_error("failed to acquire camera: " + std::to_string(result));
-        }
-    }
-
-    ~IMX290Camera() {
-        if (cameraManager_) {
-            cameraManager_->stop();
-        }
-
-        if (camera_) {
-            camera_->stop();
-            camera_->release();
-        }
-    }
-};
-
 int main() {
-    IMX290Camera camera;
-
+    std::string command = "rpicam-vid -t 0 --framerate 60 --width " + std::to_string(OLED_WIDTH) + " --height " + std::to_string(OLED_HEIGHT) + " --awbgains 0.1,1,0.1 --denoise auto -t 0";
+    system(command.c_str());
     return 0;
 }
