e96bd434b1e5934679a8fc3dcc5391f2e26846f2
mattgarabedian2015@gmail.com <130177336+matthewg-rev@users.noreply.github.com>
2026-03-29 02:32:54 -0700

    Added new testing configuration

--- src/main.py
+++ src/main.py
@@ -3,18 +3,27 @@ from picamera2 import Picamera2, Preview, controls
 import time
 
 picam2 = Picamera2()
-camera_config = picam2.create_preview_configuration(main={"size": (1920, 1080)})
-picam2.configure(camera_config)
+picam2.video_configuration.main.size = (1920, 1080)
+picam2.video_configuration.controls.FrameRate = 60
+picam2.video_configuration.lores = {"size": (1920, 1080), "format": "YUV420"}
+picam2.video_configuration.display = "lores"
+picam2.configure("video")
 
 picam2.start_preview(Preview.DRM, width=1920, height=1080)
 picam2.start()
-time.sleep(2)
 picam2.set_controls({
-    "ExposureTime": 16000,
-    'AeEnable': True,
-    'AeExposureMode': libcamera.controls.AeExposureModeEnum.Normal,
-    'AeConstraintMode': libcamera.controls.AeConstraintModeEnum.Highlight,
-    'NoiseReductionMode': libcamera.controls.draft.NoiseReductionModeEnum.HighQuality
+    "AnalogueGain": 30,
+    "ExposureTime": 16600,
+    "Sharpness": 1.0,
+    "Brightness": 0.4,
+    "Contrast": 1,
+    "Saturation": 0,
+    "ColourGains": (32.0, 32.0),
+    "NoiseReductionMode": libcamera.controls.draft.NoiseReductionModeEnum.HighQuality,
+    "HdrMode": libcamera.controls.draft.HdrModeEnum.SingleExposure,
+    "AeMeteringMode": libcamera.controls.draft.AeMeteringModeEnum.Spot,
+    "AeEnable": False,
+    "AwbEnable": False
 })
 time.sleep(1)
 
