85ff52913cc9cde17a3a1e8f0a73f0d393bd12be
mattgarabedian2015@gmail.com <130177336+matthewg-rev@users.noreply.github.com>
2026-03-29 01:07:54 -0700

    Trying test python script.

---
+++ src/main.py
@@ -0,0 +1,29 @@
+from picamera2 import Picamera2, Preview, controls
+import time
+
+picam2 = Picamera2()
+camera_config = picam2.create_preview_configuration(main={"size": (1920, 1080)})
+picam2.configure(camera_config)
+
+picam2.start_preview()
+time.sleep(2)
+picam2.set_controls({
+    "Denoise": "night",
+    "ExposureTime": 16000,
+    'AeEnable': True,
+    'AeExposureMode': controls.AeExposureModeEnum.Normal,
+    'AeConstraintMode': controls.AeConstraintModeEnum.Highlight
+})
+
+stopped_gracefully = False
+try:
+    while True:
+        time.sleep(0.001) # A small sleep to prevent a tight loop
+except KeyboardInterrupt:
+    picam2.stop_preview()
+    picam2.stop()
+    stopped_gracefully = True
+
+if not stopped_gracefully:
+    picam2.stop_preview()
+    picam2.stop()
